aaf76f7196eb09197b5155fb0bdfc57e566200cb
Michaylah Mam <mchylh@gmail.com>
2020-02-22 03:16:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,17 +18,20 @@ WIN_COMBINATIONS = [
 [6,4,2] # right_diagonal
 ]
 
-#who won?
+#winning combo 
 def won?(board)
- WIN_COMBINATIONS.each do |win_combination|
-   win_index_1 = win_combination[0]
-   win_index_2 = win_combination[1]
-   win_index_3 = win_combination[2]
-   position_1 = board[win_index_1] # value of board at win_index_1
-   position_2 = board[win_index_2] # value of board at win_index_2
-   position_3 = board[win_index_3] # value of board at win_index_3
-   position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
- end
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      position_1 = board[win_index_1] # value of board at win_index_1
+      position_2 = board[win_index_2] # value of board at win_index_2
+      position_3 = board[win_index_3] # value of board at win_index_3
+      if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+        return win_combination
+      end
+    end
+    return false
 end
 #position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
 #The above code means to return first element (position_1) & make sure the position is taken by X or O
